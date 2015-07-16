19884df4c43cbd6c0c6663c18ccc6c63343911d2
Rob <grovesr1@yahoo.com>
2015-07-16 08:06:26 -0400

    fix: change settings path in wsgi.py

--- redcross_wcny/wsgi.py
+++ redcross_wcny/wsgi.py
@@ -9,6 +9,6 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 import sys
 sys.path.append("/home/grovesr/.virtualenvs/ims/local/lib/python2.7/site-packages")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "redcross.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "redcross_wcny.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
