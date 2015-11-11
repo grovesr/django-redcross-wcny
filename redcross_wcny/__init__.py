b1a629656a19e6b0d2b6092a920b4c7a19f1cecb
Rob <grovesr1@yahoo.com>
2015-11-11 07:29:21 -0500

    add help on product code explanation and generation

--- redcross_wcny/__init__.py
+++ redcross_wcny/__init__.py
@@ -2,4 +2,5 @@
 # date format = dd/mm/YYYY
 # comment will be used to add comment to GIT tag
 #VERSION=('1.2.0','02/11/2015','add version, admin. server settings')
-VERSION=('1.3.0','XXX','add date and comment to version')
+#VERSION=('1.3.0','XXX','add date and comment to version')
+VERSION=('1.3.1','XXX','add help on product code explanation and generation')
